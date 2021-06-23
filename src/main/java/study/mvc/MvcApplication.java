4fe989306465d9a10c6bec8717ed737d0f65ce65
Jealyang <jaelyangchoi@gmail.com>
2021-06-23 17:24:56 +0900

    Initial Commit

--- src/main/java/study/mvc/MvcApplication.java
+++ src/main/java/study/mvc/MvcApplication.java
@@ -2,12 +2,14 @@ package study.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
 public class MvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MvcApplication.class, args);
+    }
 
 }
