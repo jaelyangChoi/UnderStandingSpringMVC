6f188e67ff967136b1c5fa3e99c4415bf631530d
Jealyang <jaelyangchoi@gmail.com>
2021-06-27 22:01:47 +0900

    주석 추가

--- src/main/java/study/mvc/web/frontcontroller/v5/MyHandlerAdapter.java
+++ src/main/java/study/mvc/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public interface MyHandlerAdapter {
+    //어댑터가 해당 컨트롤러를 처리할 수 있는지 판단
     boolean supports(Object handler);
 
+    //어댑터는 컨트롤러를 호출하고, 그 결과로 ModelView를 반환해야 한다.
     ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
 }
